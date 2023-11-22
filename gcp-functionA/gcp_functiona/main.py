33527f14003d9cffc82d43d73ca654fe9b52afd3
Enrico <moscaen@gmail.com>
2023-11-22 22:48:54 +0100

    feat: added docker to gcp functionA, getting Client not Authenticated error when attaching debug task

--- gcp-functionA/gcp_functiona/main.py
+++ gcp-functionA/gcp_functiona/main.py
@@ -4,11 +4,18 @@ from flask import jsonify
 import functions_framework
 
 
+import debugpy
+
+debugpy.listen(("0.0.0.0", 5678))
+debugpy.wait_for_client()
+debugger = 1  # Add this line
+
+
 @functions_framework.http
 def listen(request):
     # Assuming functionB is exposed as an HTTP trigger
-    response = requests.get("http://localhost:8080", timeout=10)
-    result_from_B = response.json()["result_from_B"]
+    # response = requests.get("http://localhost:8080", timeout=10)
+    result_from_B = "B"  # response.json()["result_from_B"]
 
     return jsonify(
         {"result_from_A": "This is from functionA", "result_from_B": result_from_B}
